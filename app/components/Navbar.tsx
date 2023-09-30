7f27d670901545eda386edb4ea4c7d71fae22fd9
Angelo Ilardi <ange448@gmail.com>
2023-09-30 21:18:34 +0100

    modified navbar to match design

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,51 +1,90 @@
 "use client";
 
-import { Navbar } from "flowbite-react";
-// import toggleIcon from './../../public/images/icon-menu.svg';
+import { useState } from "react";
 
-const toggleIcon = () => (
-  <svg width="40" height="17" xmlns="http://www.w3.org/2000/svg">
-    <g fill="#00001A" fill-rule="evenodd">
-      <path d="M0 0h40v3H0zM0 7h40v3H0zM0 14h40v3H0z" />
-      <path d="M0 0h40v3H0z" />
-    </g>
-  </svg>
-);
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "new",
+    title: "New",
+  },
+  {
+    id: "popular",
+    title: "Popular",
+  },
+  {
+    id: "trending",
+    title: "Trending",
+  },
+  {
+    id: "categories",
+    title: "Categories",
+  },
+];
 
-export default function DefaultNavbar() {
+export default function Navbar2() {
+  const [active, setActive] = useState("Home");
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <Navbar>
-      <Navbar.Brand href="#">
+    <nav className="w-full flex p-4 justify-between items-center navbar">
+      {/* Logo */}
+      <img
+        alt="Web News Logo"
+        className="mr-3 h-6 sm:h-9"
+        src="./images/logo.svg"
+      />
+
+      {/* Desktop Navigation */}
+      <ul className="list-none md:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-normal cursor-pointer border-0 hover:text-soft-orange ${
+              active === nav.title ? "hover:text-soft-orange" : "text-black"
+            } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+            onClick={() => setActive(nav.title)}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      {/* Mobile Navigation */}
+      <div className="md:hidden flex flex-1 justify-end items-center">
         <img
-          alt="Web News Logo"
-          className="mr-3 h-6 sm:h-9"
-          src="./images/logo.svg"
+          src={
+            toggle ? "./images/icon-menu-close.svg" : "./images/icon-menu.svg"
+          }
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain z-2"
+          onClick={() => setToggle(!toggle)}
         />
-      </Navbar.Brand>
-      <Navbar.Toggle barIcon={toggleIcon} />
-      <Navbar.Collapse>
-        <Navbar.Link active href="#">
-          <p>Home</p>
-        </Navbar.Link>
-        <Navbar.Link href="#" className="border-0 hover:text-soft-orange">
-          <p>About</p>
-        </Navbar.Link>
-        <Navbar.Link href="#" className="border-0 hover:text-soft-orange">
-          Home
-        </Navbar.Link>
-        <Navbar.Link href="#" className="border-0 hover:text-soft-orange">
-          New
-        </Navbar.Link>
-        <Navbar.Link href="#" className="border-0 hover:text-soft-orange">
-          Popular
-        </Navbar.Link>
-        <Navbar.Link href="#" className="border-0 hover:text-soft-orange">
-          Trending
-        </Navbar.Link>
-        <Navbar.Link href="#" className="border-0 hover:text-soft-orange">
-          Categories
-        </Navbar.Link>
-      </Navbar.Collapse>
-    </Navbar>
+
+        {/* Sidebar */}
+        <div
+          className={`${
+            !toggle ? "hidden" : "flex"
+          } p-6 bg-white absolute top-16 right-0 w-[70%] h-full`}
+        >
+          <ul className="list-none flex justify-start items-start flex-1 flex-col">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`cursor-pointer text-xl ${
+                  active === nav.title ? "text-black" : "text-black"
+                } ${index === navLinks.length - 1 ? "mb-0" : "mb-8"}`}
+                onClick={() => setActive(nav.title)}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
